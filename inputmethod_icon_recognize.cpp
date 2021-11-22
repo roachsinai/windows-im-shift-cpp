32fbda9426de1db219a9e2e8e5e009e4dfd717ec
roachsinai <roachsinai@qq.com>
2021-11-22 11:43:05 +0800

    Use pixel value of IM icon area as key, use item count of map to recognize IM icon.

--- inputmethod_icon_recognize.cpp
+++ inputmethod_icon_recognize.cpp
@@ -2,11 +2,11 @@
 #define WIN32_LEAN_AND_MEAN
 #define VC_EXTRALEAN
 #include <iostream>
-#include <string>
+#include <map>
 #include <Windows.h>
 
 
-int* screenshot(int& width, int& height) {
+int* screen_shot(int& width, int& height) {
     HDC hdc = GetDC(NULL); // get the desktop device context
     HDC cdc = CreateCompatibleDC(hdc); // create a device context to use yourself
     height = (int)GetSystemMetrics(SM_CYVIRTUALSCREEN); // get the width and height of the screen
@@ -30,7 +30,6 @@ int* screenshot(int& width, int& height) {
     return image;
 }
 
-
 int main(int argc, char* argv[])
 {
     if (argc != 5 && argc != 6)
@@ -39,39 +38,27 @@ int main(int argc, char* argv[])
         return -1;
     }
     int width=0, height=0;
-    int* image = screenshot(width, height);
-
-    // access pixel colors for position (x|y)
-    // const int x=0, y=0;
-    // const int color = image[x+y*width];
-    // const int red   = (color>>16)&255;
-    // const int green = (color>> 8)&255;
-    // const int blue  =  color     &255;
-
-    // int startx = 1715;
-    // int starty = 1047;
-    // int endx = 1734;
-    // int endy = 1065;
+    int* image = screen_shot(width, height);
 
     int startx = std::stoi(argv[1]);
     int starty = std::stoi(argv[2]);
     int endx = std::stoi(argv[3]);
     int endy = std::stoi(argv[4]);
 
-    long long int count = 0;
+    std::map<int, int> m;
 
     for (int j = starty; j < endy; ++j)
     {
         for (int i = startx; i < endx; ++i)
         {
-            count += image[i + width * j];
+            ++m[image[i + width * j]];
         }
     }
     delete[] image;
 
     if (argc == 5)
-        std::cout << count << std::endl;
-    else if (count == std::stoi(argv[5]))
+        std::cout << m.size() << std::endl;
+    else if (m.size() <= std::stoi(argv[5]))
         std::cout << 2 << std::endl;
     else
         std::cout << 1 << std::endl;
